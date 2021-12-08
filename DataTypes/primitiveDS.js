c6e0cf995255265b4f0f1870844d5e1e5116e690
Chandankn0422 <chandanchandu0422@gmail.com>
2021-12-09 00:05:44 +0530

    okie

---
+++ DataTypes/primitiveDS.js
@@ -0,0 +1,15 @@
+var price = 499;
+var Product_Name ="nike";
+var is_Avail = true;
+var discount ;
+
+/*
+console.log(price)
+console.log(Product_Name)
+console.log(is_Avail)
+console.log(discount) */
+
+console.log(typeof (price))
+console.log(typeof (Product_Name))
+console.log(typeof (is_Avail))
+console.log(typeof (discount))
