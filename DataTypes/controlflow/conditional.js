aa1a6f55dcfbe264b33772ccf73364209b5151b3
Chandankn0422 <chandanchandu0422@gmail.com>
2021-12-10 07:11:17 +0530

    okie

---
+++ DataTypes/controlflow/conditional.js
@@ -0,0 +1,12 @@
+/*if and else */
+/*if (48% 2 ==0) {
+    console.log(true)
+} else {
+    console.log(false)
+}*/
+
+
+let salary = 75000
+if (salary>=75000){
+    console.log("eligible")
+} else{ console.log("not")}
---
+++ DataTypes/controlflow/ternery.js
@@ -0,0 +1,5 @@
+/*48 % 3 ==0 ?console.log("even Number") : console.log("not");*/
+/*"" ? console.log("gm") : console.log("gn") */
+
+let salary = 45000
+ salary >= 45000? console.log("eligible") : console.log("not")
